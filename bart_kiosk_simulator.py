e6745e0ba24518696bda9ede8f3e9f76a41773ce
Hackbright Student <info@hackbrightacademy.com>
2016-04-28 20:57:19 -0700

    committing this file

---
+++ bart_kiosk_simulator.py
@@ -0,0 +1,9 @@
+def load_card(num1, num5, num10, num20):
+    a = int(num1)*1
+    b = int(num5)*5
+    c = int(num10)*10
+    d = int(num20)*20
+    total = a + b + c + d
+    return total
+
+print load_card(0,0,0,9)
